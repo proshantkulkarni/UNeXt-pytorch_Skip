f4b102f70b147255a7b1efdbbc0d545387e0c17c
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-06-23 12:05:09 +0530

    debug

--- train.py
+++ train.py
@@ -267,27 +267,46 @@ def main():
     # train_img_ids = glob(os.path.join('/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic/train/images', '*' +config['img_ext']))
     # val_img_ids = glob(os.path.join('/content/drive/MyDrive/Amit-Paper3/UNeXt-pytorch/inputs/isic/val/images','*'+ config['img_ext']))
     
-    train_img_ids = glob(os.path.join(config['dataset'], 'train', 'images', '*' + config['img_ext']))
-    val_img_ids = glob(os.path.join(config['dataset'], 'val', 'images', '*' + config['img_ext']))
+    # train_img_ids = glob(os.path.join(config['dataset'], 'train', 'images', '*' + config['img_ext']))
+    # val_img_ids = glob(os.path.join(config['dataset'], 'val', 'images', '*' + config['img_ext']))
 
-    valid_train_ids = []
-    for img_id in train_img_ids:
-        img_path = os.path.join(config['dataset'], 'train', 'images', img_id + config['img_ext'])
-        mask_path = os.path.join(config['dataset'], 'train', 'masks', img_id + config['mask_ext'])
-        if os.path.exists(img_path) and os.path.exists(mask_path):
-            valid_train_ids.append(img_id)
-    train_img_ids = valid_train_ids
+    # valid_train_ids = []
+    # for img_id in train_img_ids:
+    #     img_path = os.path.join(config['dataset'], 'train', 'images', img_id + config['img_ext'])
+    #     mask_path = os.path.join(config['dataset'], 'train', 'masks', img_id + config['mask_ext'])
+    #     if os.path.exists(img_path) and os.path.exists(mask_path):
+    #         valid_train_ids.append(img_id)
+    # train_img_ids = valid_train_ids
 
-    valid_val_ids = []
-    for img_id in val_img_ids:
-        img_path = os.path.join(config['dataset'], 'val', 'images', img_id + config['img_ext'])
-        mask_path = os.path.join(config['dataset'], 'val', 'masks', img_id + config['mask_ext'])
-        if os.path.exists(img_path) and os.path.exists(mask_path):
-            valid_val_ids.append(img_id)
-    val_img_ids = valid_val_ids
+    # valid_val_ids = []
+    # for img_id in val_img_ids:
+    #     img_path = os.path.join(config['dataset'], 'val', 'images', img_id + config['img_ext'])
+    #     mask_path = os.path.join(config['dataset'], 'val', 'masks', img_id + config['mask_ext'])
+    #     if os.path.exists(img_path) and os.path.exists(mask_path):
+    #         valid_val_ids.append(img_id)
+    # val_img_ids = valid_val_ids
 
-    train_img_ids = [os.path.splitext(os.path.basename(p))[0] for p in train_img_ids]
-    val_img_ids = [os.path.splitext(os.path.basename(p))[0] for p in val_img_ids]
+    train_img_paths = glob(os.path.join(config['dataset'], 'train', 'images', '*' + config['img_ext']))
+    val_img_paths = glob(os.path.join(config['dataset'], 'val', 'images', '*' + config['img_ext']))
+
+    train_img_ids = [os.path.splitext(os.path.basename(p))[0] for p in train_img_paths]
+    val_img_ids   = [os.path.splitext(os.path.basename(p))[0] for p in val_img_paths]
+
+    train_img_ids = [
+        img_id for img_id in train_img_ids
+        if os.path.exists(os.path.join(config['dataset'], 'train', 'images', img_id + config['img_ext'])) and
+        os.path.exists(os.path.join(config['dataset'], 'train', 'masks', img_id + config['mask_ext']))
+    ]
+
+    val_img_ids = [
+        img_id for img_id in val_img_ids
+        if os.path.exists(os.path.join(config['dataset'], 'val', 'images', img_id + config['img_ext'])) and
+        os.path.exists(os.path.join(config['dataset'], 'val', 'masks', img_id + config['mask_ext']))
+    ]
+
+
+    # train_img_ids = [os.path.splitext(os.path.basename(p))[0] for p in train_img_ids]
+    # val_img_ids = [os.path.splitext(os.path.basename(p))[0] for p in val_img_ids]
 
     # train_img_ids, val_img_ids = train_test_split(img_ids, test_size=0.1, random_state=41)
 
