a5cccde8b731b6cb09ce8eff56c08c328e125817
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-06-27 15:16:26 +0530

    DCA_early

--- train.py
+++ train.py
@@ -26,13 +26,13 @@ from metrics import iou_score
 from utils import AverageMeter, str2bool
 
 # from archs_semantic_map import UNext
-import archs_CTrans
+# import archs_CTrans
 # import archs
-# import archs_DCA
+import archs_DCA
 
 # ARCH_NAMES = archs.__all__
-# ARCH_NAMES = archs_DCA.__all__
-ARCH_NAMES = archs_CTrans.__all__
+ARCH_NAMES = archs_DCA.__all__
+# ARCH_NAMES = archs_CTrans.__all__
 LOSS_NAMES = losses.__all__
 LOSS_NAMES.append('BCEWithLogitsLoss')
 
@@ -299,16 +299,16 @@ def main():
     cudnn.benchmark = True
 
     # create model
-    # model = archs_DCA.__dict__[config['arch']](config['num_classes'],
-                                        #    config['input_channels'],
-                                        #    config['deep_supervision'])
+    model = archs_DCA.__dict__[config['arch']](config['num_classes'],
+                                           config['input_channels'],
+                                           config['deep_supervision'])
 
     # model = archs.__dict__[config['arch']](config['num_classes'],
                                         #    config['input_channels'],
                                         #    config['deep_supervision'])
-    model = archs_CTrans.__dict__[config['arch']](config['num_classes'],
-                                           config['input_channels'],
-                                           config['deep_supervision'])
+    # model = archs_CTrans.__dict__[config['arch']](config['num_classes'],
+    #                                        config['input_channels'],
+    #                                        config['deep_supervision'])
     # model = model.cuda()
     model = model.to(device)
 
