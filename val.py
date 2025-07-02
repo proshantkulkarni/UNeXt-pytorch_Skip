105f1d291d3f1f78ca336db3c7c1c395be678742
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-07-02 23:20:02 +0530

    arch DCA wavelet

--- val.py
+++ val.py
@@ -16,8 +16,8 @@ from tqdm import tqdm
 # import archs_DCA
 # import archs_CTrans_wavelet
 # import archs_Fusion
-# import archs_wavelet
-import archs_DCA_wavelet
+import archs_wavelet
+# import archs_DCA_wavelet
 
 from dataset import Dataset
 from metrics import iou_score
@@ -103,9 +103,9 @@ def main():
 
     # model = archs_Fusion.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision']).cuda()
 
-    # model = archs_wavelet.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision']).cuda()
+    model = archs_wavelet.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision']).cuda()
 
-    model = archs_DCA_wavelet.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision']).cuda()
+    # model = archs_DCA_wavelet.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision']).cuda()
 
     # model.load_state_dict(torch.load('/content/drive/MyDrive/Amit-Paper3/ISIC_1_original/isic_exp/model.pth', weights_only=True))
     model.load_state_dict(torch.load(model_path))
