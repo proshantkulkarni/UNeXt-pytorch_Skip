9003e92e89cfc255f64c78efaefad5b54ea3e07e
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-07-28 14:47:35 +0530

    inception atto

--- train.py
+++ train.py
@@ -29,14 +29,17 @@ from utils import AverageMeter, str2bool
 # import archs_CTrans
 # ARCH_NAMES = archs_CTrans.__all__
 
-import archs
-ARCH_NAMES = archs.__all__
+# import archs
+# ARCH_NAMES = archs.__all__
 
 # import archs_MLFC
 # ARCH_NAMES = archs_MLFC.__all__
 
 # import archs_InceptionNext
 # ARCH_NAMES = archs_InceptionNext.__all__
+
+import archs_InceptionNext_atto
+ARCH_NAMES = archs_InceptionNext_atto.__all__
 
 # import archs_ConvNext
 # ARCH_NAMES = archs_ConvNext.__all__
@@ -341,7 +344,13 @@ def main():
     #     config['deep_supervision']
     # ).to(device)
 
-    model = archs.__dict__[config['arch']](
+    # model = archs.__dict__[config['arch']](
+    #     config['num_classes'],
+    #     config['input_channels'],
+    #     config['deep_supervision']
+    # ).to(device)
+
+    model = archs_InceptionNext_atto.__dict__[config['arch']](
         config['num_classes'],
         config['input_channels'],
         config['deep_supervision']
