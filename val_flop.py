9d0771127c13c0cf8b6ec6f19bc523b4c7764e85
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-07-28 12:55:54 +0530

    flop calculation

--- val_flop.py
+++ val_flop.py
@@ -12,9 +12,9 @@ from tqdm import tqdm
 from thop import profile
 import time
 
-# import archs
+import archs
 # import archs_MLFC
-import archs_InceptionNext
+# import archs_InceptionNext
 # import archs_ConvNext
 # import archs_CTrans
 # import archs
@@ -102,12 +102,12 @@ def main():
     print(f"=> creating model {config['arch']}")
 
 
-    # model = archs.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision']).cuda()
+    model = archs.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision']).cuda()
 
     # model = archs_MLFC.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision']).cuda()
 
-    model = archs_InceptionNext.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision']).cuda()
-
+    # model = archs_InceptionNext.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision']).cuda()
+# 
     # model = archs_ConvNext.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision']).cuda()
 
     # model = archs_topformer.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision']).cuda()
