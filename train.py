0c20a86510b5adcd6c9945f848f8ab206a1779f2
Prashant Dhananjay Kulkarni <VE00YN139@YMI.COM>
2025-07-29 17:18:44 +0530

    MedMamba

--- train.py
+++ train.py
@@ -41,8 +41,11 @@ from utils import AverageMeter, str2bool
 # import archs_InceptionNext_SE
 # ARCH_NAMES = archs_InceptionNext_SE.__all__
 
-import archs_InceptionNext_MLFC
-ARCH_NAMES = archs_InceptionNext_MLFC.__all__
+# import archs_InceptionNext_MLFC
+# ARCH_NAMES = archs_InceptionNext_MLFC.__all__
+
+import archs_medmamba
+ARCH_NAMES = archs_medmamba.__all__
 
 # import archs_InceptionNext_atto
 # ARCH_NAMES = archs_InceptionNext_atto.__all__
@@ -368,7 +371,13 @@ def main():
     #     config['deep_supervision']
     # ).to(device)
 
-    model = archs_InceptionNext_MLFC.__dict__[config['arch']](
+    # model = archs_InceptionNext_MLFC.__dict__[config['arch']](
+    #     config['num_classes'],
+    #     config['input_channels'],
+    #     config['deep_supervision']
+    # ).to(device)
+
+    model = archs_medmamba.__dict__[config['arch']](
         config['num_classes'],
         config['input_channels'],
         config['deep_supervision']
